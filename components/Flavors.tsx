ec698b15a3e93d90d182386e38ca542b6e5dd014
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-04-08 12:55:57 +0100

    Animate flavors texts using scrollTrigger

--- components/Flavors.tsx
+++ components/Flavors.tsx
@@ -1,16 +1,50 @@
+"use client";
 import React from "react";
 import TextSplitter from "@/components/TextSplitter";
 import Image from "next/image";
 import cansImage from "@/public/img/cans.png";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 function Flavors() {
+  useGSAP(() => {
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".page-wrapper",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTl
+      .from(".flavors-title .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".flavors-body", {
+        y: 20,
+        opacity: 0,
+      });
+  });
   return (
     <div className="text-side relative z-[10] grid h-screen items-center gap-4 md:grid-cols-2">
       <div>
         <h2 className="text-side-heading text-6xl font-black text-balance text-sky-950 uppercase lg:text-8xl">
-          <TextSplitter text={"Try all five flavors"} />
+          <TextSplitter
+            className="flavors-title"
+            text={"Try all five flavors"}
+          />
         </h2>
-        <div className="text-side-body mt-4 max-w-xl text-xl font-normal text-balance text-sky-950">
+        <div className="flavors-body text-side-body mt-4 max-w-xl text-xl font-normal text-balance text-sky-950">
           Our soda is made with real fruit juice and a touch of cane sugar. We
           never use artificial sweeteners or high fructose corn syrup. Try all
           five flavors and find your favorite!
@@ -19,13 +53,12 @@ function Flavors() {
       <div className="relative h-full w-full">
         <Image
           src={cansImage}
-          alt="About Us"
+          alt="All flavors"
           fill
-          sizes="100vw"
+          sizes="100"
           quality={100}
           priority
           className="object-cover"
-          placeholder="blur"
         />
       </div>
     </div>
