28bb3331964586f3077a9774acd131e4b3f8aa99
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-04-09 12:30:01 +0100

    Simplify BoundedContainer: Remove dynamic element type

--- components/BoundedContainer.tsx
+++ components/BoundedContainer.tsx
@@ -1,26 +1,20 @@
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: React.ElementType;
+interface BoundedProps {
   className?: string;
   children: React.ReactNode;
-};
+}
 
-function Bounded({
-  as: Comp = "section",
-  className,
-  children,
-  ...restProps
-}: BoundedProps) {
+function Bounded({ className, children, ...restProps }: BoundedProps) {
   return (
-    <Comp
+    <section
       className={clsx("px-4 first:pt-10 md:px-6", className)}
       {...restProps}
     >
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
         {children}
       </div>
-    </Comp>
+    </section>
   );
 }
 
